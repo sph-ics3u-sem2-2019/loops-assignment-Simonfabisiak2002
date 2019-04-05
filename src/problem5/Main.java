df088e0f2097fea9c6e5d996487d0b995b1745f2
Simonfabisiak2002 <simon.fabisiak@ocsbstudent.ca>
2019-04-05 08:54:39 -0400

    good

--- src/problem5/Main.java
+++ src/problem5/Main.java
@@ -5,6 +5,25 @@ public class Main {
 	public static void main(String[] args) {
 		// Read the string and output in reverse case.
 		Scanner in=new Scanner(System.in);
-	}
+		String b =in.nextLine();
+		for (int x=0;x<b.length();x++) {
+		char letter=b.charAt(x);
+		boolean upper= Character.isUpperCase(letter);
+		boolean lower= Character.isLowerCase(letter);
+if (upper==true) {
+	letter=Character.toLowerCase(letter);
+	System.out.print(letter); }
+else if (lower==true) {
+	letter=Character.toUpperCase(letter);
+	System.out.print(letter);
+}
+else {System.out.print(letter);}
+			}
 
+		
+			
+
+		
+	//System.out.print(b.substring(b));//
+	}
 }
